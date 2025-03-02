d2b7a36c3aa82722e807cd88706c51115543332c
Leon Farafonov <117378906+leofaraf@users.noreply.github.com>
2025-03-02 21:46:40 +0300

    feat: add explicit features

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub fn include_secure_str(_item: TokenStream) -> TokenStream {
     include_secure_str::handle(_item)
 }
 
+#[cfg(feature = "brotli")]
 #[proc_macro]
 pub fn secure_str_brotli(_item: TokenStream) -> TokenStream {
     secure_str::brotli(_item)
--- tests/secure_str.rs
+++ tests/secure_str.rs
@@ -1,7 +1,10 @@
+#[cfg(feature = "brotli")]
 use secure::secure_str_brotli;
 
+#[cfg(feature = "brotli")]
 secure_str_brotli!("brotli_value", "decompressed");
 
+#[cfg(feature = "brotli")]
 #[test]
 fn secure_str_brotli() {
     assert_eq!(get_brotli_value(), "decompressed".to_string());
