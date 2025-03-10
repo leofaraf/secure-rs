75b7e69d9121c97a648bb59d66ec655e2e89458b
Leon Farafonov <117378906+leofaraf@users.noreply.github.com>
2025-03-10 18:48:22 +0300

    docs: aes macros methods

--- src/lib.rs
+++ src/lib.rs
@@ -47,18 +47,45 @@ pub fn include_secure_bytes_brotli(_item: TokenStream) -> TokenStream {
 
 #[cfg(feature = "encryption")]
 #[proc_macro]
+/// **pathname: &str, aes_key: &str, varname: &str**
+/// 
+/// equivalent of `include_str!()`, but compress in compile-time via brotli
+/// 
+/// pathname is path of located file
+/// 
+/// aes_key is string that should contain utf8 key, the number of bytes of the key shall be 32
+/// 
+/// each macro accept varname and as result generate function with name: `get_{varname}`
 pub fn include_secure_str_aes(_item: TokenStream) -> TokenStream {
     include_secure_str::aes(_item)
 }
 
 #[cfg(feature = "encryption")]
 #[proc_macro]
+/// aes_key: &str, **varname: &str, value: &str**
+/// 
+/// equivalent of `include_bytes!()`, but compress in compile-time via brotli
+/// 
+/// value is result fo generated function
+/// 
+/// aes_key is string that should contain utf8 key, the number of bytes of the key shall be 32
+/// 
+/// each macro accept varname and as result generate function with name: `get_{varname}`
 pub fn secure_str_aes(_item: TokenStream) -> TokenStream {
     secure_str::aes(_item)
 }
 
 #[cfg(feature = "encryption")]
 #[proc_macro]
+/// **pathname: &str, aes_key: &str, varname: &str**
+/// 
+/// equivalent of `include_bytes!()`, but compress in compile-time via brotli
+/// 
+/// pathname is path of located file
+/// 
+/// aes_key is string that should contain utf8 key, the number of bytes of the key shall be 32
+/// 
+/// each macro accept varname and as result generate function with name: `get_{varname}`
 pub fn include_secure_bytes_aes(_item: TokenStream) -> TokenStream {
     include_secure_bytes::aes(_item)
 }
