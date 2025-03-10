6b1a7bb5e3dfa29ff7d610d989511d8ea57571d1
Leon Farafonov <117378906+leofaraf@users.noreply.github.com>
2025-03-10 18:43:44 +0300

    docs: brotli macros methods

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,39 @@ use proc_macro::TokenStream;
 
 #[cfg(feature = "compression")]
 #[proc_macro]
+/// **pathname: &str, varname: &str**
+/// 
+/// equivalent of `include_str!()`, but compress in compile-time via brotli
+/// 
+/// pathname is path of located file
+/// 
+/// each macro accept varname and as result generate function with name: `get_{varname}`
 pub fn include_secure_str_brotli(_item: TokenStream) -> TokenStream {
     include_secure_str::brotli(_item)
 }
 
 #[cfg(feature = "compression")]
 #[proc_macro]
+/// **varname: &str, value: &str**
+/// 
+/// equivalent of `include_bytes!()`, but compress in compile-time via brotli
+/// 
+/// value is result fo generated function
+/// 
+/// each macro accept varname and as result generate function with name: `get_{varname}`
 pub fn secure_str_brotli(_item: TokenStream) -> TokenStream {
     secure_str::brotli(_item)
 }
 
 #[cfg(feature = "compression")]
 #[proc_macro]
+/// **pathname: &str, varname: &str**
+/// 
+/// equivalent of `include_bytes!()`, but compress in compile-time via brotli
+/// 
+/// pathname is path of located file
+/// 
+/// each macro accept varname and as result generate function with name: `get_{varname}`
 pub fn include_secure_bytes_brotli(_item: TokenStream) -> TokenStream {
     include_secure_bytes::brotli(_item)
 }
